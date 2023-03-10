86552ed5569e38cdeb5a2155ebff4c30b92cca65
DrewMax <andrew.reviresco@gmail.com>
2023-03-10 15:50:08 +0100

    LEFT JOIN candidates --bugs--

--- server.js
+++ server.js
@@ -24,8 +24,12 @@ app.use(express.json());
 
 // GET a single candidate
 app.get('./api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates
-            WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
@@ -54,7 +58,12 @@ db.run(sql, params, function(err, result) {
 
 //Delete a candidate 
 app.delete("/api/candidate/:id", (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db:run(sql, params, function(err, result) {
         if (err) {
@@ -96,7 +105,11 @@ app.post('/api/candidate', ({ body }, res) => {
 
 //Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
     const params = [req.params];
     db.all(sql, params, (err, rows) => {
         if (err) {
